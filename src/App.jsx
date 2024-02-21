2d7fb9ff56aa481e86e6c8f03454f098a2d0f5dd
joaopedropicolo <bigwon132@gmail.com>
2024-02-21 15:51:24 -0400

    Adicionado outro nome

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 export default function App() {
 
-  const nome = "Picolo";
+  const nome = "João Santos";
   
   return(
     <div>
-        <h1>Olá Menino De Papai</h1>
+        <h1>Hello World</h1>
         <p>Desenvolvido por: {nome}</p>
     </div>  
   );
 
+  const picolo = "Picolo";
+  return(
+    <div>
+        <h1>Olá Mundo</h1>
+        <p>Desenvolvido por: {nome-picolo}</p>
+    </div>  
+  );
 
 }
