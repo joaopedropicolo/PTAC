49b5772147318fca4a13ed0a0e8fc893fcd74240
joaopedropicolo <bigwon132@gmail.com>
2024-02-28 16:06:21 -0400

    Feito componete Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import Header from "./components/Header";
 export default function App() {
 
-  const nome = "João Santos";
-  const picolo = "Picolo Brabão dos códigos";
-
   return(
-    <div>
-        <h1>Hello World</h1>
-        <p>Desenvolvido por: {nome}</p>
-        <p>Made Bye: {picolo}</p>
-    </div> 
+    <main>
+      <Header/>
+      <Header/>
+      <Header/>
+      <Header/>
+    </main>
   );
 
 }
