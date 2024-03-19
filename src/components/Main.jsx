565fdfde7a583330cb47f0ae34880711c9cc31ff
joaopedropicolo <bigwon132@gmail.com>
2024-03-18 21:55:06 -0300

    Adicionado {nome} e {telefone}

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,11 +10,14 @@ export default function Main(){
         <form>
             <label htmlFor="nome">Nome: </label>
             <input type="text" name="" id="" placeholder="Seu Nome" onChange={(event)=> setNome(event.target.value)}/>
-            <br></br><br></br>
-           <label htmlFor="telefone">Telefone: </label>
-           <input type="tel" id="telefone" value={telefone} placeholder="Seu Telefone" onChange={(event) => setTelefone(event.target.value)}/>
-           <br></br><br></br>
-            <button type="submit">Enviar</button> 
+            <br></br>
+            {nome}
+            <br></br>
+            <label htmlFor="telefone">Telefone: </label>
+            <input type="tel" id="telefone" value={telefone} placeholder="Seu Telefone" onChange={(event) => setTelefone(event.target.value)}/>
+            <br></br>
+            {telefone}
+           <br></br>
         </form>
         </main>
     );
