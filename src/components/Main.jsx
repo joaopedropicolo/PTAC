d4cc7497f3fa17f44c2c16d27a349f2393088b48
joaopedropicolo <bigwon132@gmail.com>
2024-03-20 19:40:43 -0300

    Foi colocado a visualização dos elementos
    Foi readicionado a visualização dos elementos inseridos nas inputs.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,9 +10,9 @@ export default function Main(){
         <main>
             <div id="bloco-insersoes">
         <form>
-            <label htmlFor="nome">Nome: </label>
+            <label htmlFor="nome">Nome: </label> {nome}
             <div className="padding-input"><input type="text" name="" id="" placeholder="Seu Nome" onChange={(event)=> setNome(event.target.value)}/></div>
-            <label htmlFor="telefone">Telefone: </label>
+            <label htmlFor="telefone">Telefone: </label> {telefone}
             <div className="padding-input"><input type="tel" id="telefone" value={telefone} placeholder="Seu Telefone" onChange={(event) => setTelefone(event.target.value)}/></div>
            <button>Enviar</button>
         </form>
