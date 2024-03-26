35194712093f6725bc665522635bbdebbdbdb072
joaopedropicolo <bigwon132@gmail.com>
2024-03-25 21:47:20 -0300

    Feito update de funções e estilização

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 export default function Footer(){
     return(
         <footer>
-            <p>Desenvolvido por Picolo©️</p>
+            <p>Desenvolvido por Picolo©️
+            <a href="https://github.com/joaopedropicolo/PTAC-Aula02"><ion-icon name="logo-github"></ion-icon></a>
+            <a href="https://www.instagram.com/big.w0n?igsh=aXpvYTd2aHk3bWxp"><ion-icon name="logo-instagram"></ion-icon></a>
+            <a href="https://steamcommunity.com/id/bigwon/"><ion-icon name="logo-steam"></ion-icon></a>
+            </p>
         </footer>
     );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,17 +4,46 @@ export default function Main(){
 
     const [nome, setNome] = useState("");
     const [telefone, setTelefone] = useState("");
+    const [listaContatos, setListaContatos] = useState ([]);
+    const salvar = function (){
+        if(nome == "" || telefone == ""){
+            console.log("O usuário não inseriu alguma das informações.");
+            Swal.fire({
+                title: "ERRO",
+                text: "Não foram inseridos algumas das informações!",
+                icon: "error"
+              });
+        }
+        else{
+            console.log("O nome do usuário é:", nome, "e seu telefone é:", telefone);
+            Swal.fire({
+                title: "Contato Salvo!",
+                text: "Seu contato foi salvo com sucesso!",
+                icon: "success"
+              });
+            registrar();
+        }
+    }
+    const registrar = () => {
+        console.table(listaContatos);
+        setListaContatos([listaContatos,
+        {
+            nomeSalvo: nome,
+            telefoneSalvo: telefone
+        }
+        ])
+    };
 
     return(
         <div id="centralizacao-main">
         <main>
             <div id="bloco-insersoes">
         <form>
-            <label htmlFor="nome">Nome: </label> {nome}
-            <div className="padding-input"><input type="text" name="" id="" placeholder="Seu Nome" onChange={(event)=> setNome(event.target.value)}/></div>
-            <label htmlFor="telefone">Telefone: </label> {telefone}
+            <label htmlFor="nome">Nome: </label>
+            <div className="padding-input"><input type="text" name="" id="nome" placeholder="Seu Nome" onChange={(event)=> setNome(event.target.value)}/></div>
+            <label htmlFor="telefone">Telefone: </label>
             <div className="padding-input"><input type="tel" id="telefone" value={telefone} placeholder="Seu Telefone" onChange={(event) => setTelefone(event.target.value)}/></div>
-           <button>Enviar</button>
+           <button type="button" onClick={salvar}>Enviar</button>
         </form>
             </div>
         </main>
