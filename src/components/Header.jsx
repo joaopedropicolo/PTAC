a19aa99fcdcb356bb16a85f15f5cbf0f1601d8c7
joaopedropicolo <bigwon132@gmail.com>
2024-03-27 11:23:33 -0300

    Feito estilização

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 export default function Header(){
     return(
         <header>
-            <p>AGENDA TELEFÔNICA</p>
+            <div id="alinhar-header">
+            <p id="paragrafo-header"><ion-icon id="call-icon" name="call"></ion-icon>AGENDA TELEFÔNICA</p>
+            </div>
         </header>
     );
 }
