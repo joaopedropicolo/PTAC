8ca7ac9b3152ce778143fd7261d04c3b05f67f8b
joaopedropicolo <bigwon132@gmail.com>
2024-05-29 21:57:45 -0300

    Feito button para a remoção dos contatos

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -33,6 +33,33 @@ const registrar = () => {
             console.log("As informações salvas são: Nome", nome, "Apelido:", apelido, "Telefone:", telefone);
         }
     }
+    const remover = (id) => {
+        const novaLista = listaContatos.filter(
+            (contato,index)=>{
+                if(index !== id){
+                    return contato
+                } else {
+                    return null;
+                }
+            }
+        )
+        Swal.fire({
+            icon: "warning",
+            title: "Tem certeza que quer remover o contato?",
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: "Cancelar",
+            denyButtonText: `Remover`
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+              Swal.fire("Ação Cancelada!", "", "success");
+            } else if (result.isDenied) {
+              Swal.fire("Contato Removido!", "", "success");
+              setListaContatos(novaLista);
+            }
+          });
+        }
     return(
         <main>
             <div id="centralizacao-main">
@@ -49,10 +76,10 @@ const registrar = () => {
         </div>
             </div>
             <div id="centralizacao-salvos">
-            <div class="bloco-insersoes">
-                <h3>Contatos Salvos:</h3>
-            {listaContatos.map((contato, index)=> <p key={index}><h4 class="h4-salvos">Nome:</h4> <p class="p-salvos">{contato.nomeSalvo}</p> <h4 class="h4-salvos">Apelido:</h4> <p class="p-salvos">{contato.apelidoSalvo}</p> <h4 class="h4-salvos">Telefone:</h4> <p class="p-salvos">{contato.telefoneSalvo}</p><p class="linha"></p></p>)}
-            </div>
+                <div class="bloco-insersoes">
+            <h3>Contatos Salvos:</h3>
+                {listaContatos.map((contato, index)=> <p key={index}><h4 class="h4-salvos">Nome:</h4> <p class="p-salvos">{contato.nomeSalvo}</p> <h4 class="h4-salvos">Apelido:</h4> <p class="p-salvos">{contato.apelidoSalvo}</p> <h4 class="h4-salvos">Telefone:</h4> <p class="p-salvos">{contato.telefoneSalvo}</p><button id="remover-contato" onClick={()=> remover(index)}>X</button><p class="linha"></p></p>)}
+                </div>
             </div>
         </main>
     );
